829e23067c52e13d6d42770ebcac2076b80f6ad3
juan-mendez <juan-mendez7@upc.edu.co>
2021-01-26 18:45:27 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/edu/myserviciosrestapi/PostService.java
@@ -0,0 +1,11 @@
+package co.edu.unipiloto.edu.myserviciosrestapi;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PostService {
+    String API_ROUTE="/posts";
+    @GET(API_ROUTE)
+    Call<List<PostClass>>getPost();
+}
